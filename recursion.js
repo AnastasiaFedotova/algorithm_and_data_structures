f294a243bdf06504cbfd8003b58ba1ecb8134930
Anastasia <Anastasia@MacBook-Air-Ila.local>
2019-10-30 22:57:37 +0300

    changed

--- recursion.js
+++ recursion.js
@@ -1,12 +1,17 @@
 //применение рекурсии в подсчете суммы всех чисел массива
-function summ(arr, index) {
+function summ(arr, index = 0) {
+    if(arr.length < 2) return arr[index] || false;
     if(arr.length - 1 != index) {
-        return arr[index] += summ(arr, index+1);
+        return arr[index] += summ(arr, index + 1);
     }
     return arr[index];
 }
 summ([20, 30, 4], 0);
 
-/*/найти наибольшее число в массиве
-function findBiggest(arr) {
-}*/
+//найти наибольшее число в массиве
+function findBiggest(arr, index = 0, count) {
+    if(arr.length < 2) return arr[index] || false;
+    if(index == arr.length - 1) return count;
+    arr[index] > count ? count = arr[index] : false;
+    return findBiggest(arr, index += 1, count);
+}

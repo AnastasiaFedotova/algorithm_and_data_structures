8d537f59c63c8b7d9075876990d86ad99eb59852
Anastasia <Anastasia@MacBook-Air-Ila.local>
2019-11-13 23:48:28 +0300

    add

--- linked_list.js
+++ linked_list.js
@@ -1,8 +1,8 @@
 /*Реализовать связанный список*/
 class Item {
-    constructor(value, link) {
+    constructor(value, next) {
         this.value = value;
-        this.link = link;
+        this.next = next;
     }
 }
 
@@ -16,16 +16,38 @@ class LinkedList {
         if(!this.start) {
             this.end = new Item(value, null);
             this.start = this.end;
-            return this.end;
-        } else if(this.start != null && this.start == this.end) {
-            this.end = new Item(value, null) 
-            this.start.link = this.end;
-            return this.end;
+        } else {
+	        let gup = new Item(value, null)
+            this.end.next = gup;
+	        this.end = gup;
+	    }
+    }
+
+    get(index) {
+        if(index < 0 || !isFinite(index) || index == undefined) return;
+        let item = this.start;
+        for(let i = 0; i < index; i++) {
+            if(item.next == null) return;
+            item = item.next;
         }
-        this.end.link = new Item(value, null);
-        return this.end.link;
+        return item;
+    }
+
+    remove(index) {
+        if(this.get(index) == this.end) {
+            this.end == this.get(index - 1);
+        } else if(this.get(index) == this.start) {
+            this.start = this.get(index + 1);
+        } else if(this.get(index)) {
+            this.get(index - 1).next = this.get(index).next;
+        }
     }
 }
 
 let list = new LinkedList();
-list.add();
+list.add('Я первый');
+list.add(2);
+list.add(3);
+list.add('Я четвертый');
+list.add('Я пятый');
+list.add('6');
